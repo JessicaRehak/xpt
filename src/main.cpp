b5cce5c616ffcaa47a3ac894efc5a68064a883bc
Joshua Rehak <jsrehak@gmail.com>
2017-11-15 16:24:18 -0800

    removed extra main file, added macros for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+#ifdef TEST
+#include "gtest/gtest.h"
+#endif
+
 int main(int argc, char* argv[]) {
+#ifdef TEST
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+#else
+  std::cout << "XPT!" <<std::endl;
   return 0;
+#endif
 }
 
